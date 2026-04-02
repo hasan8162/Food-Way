d3bb72f7c5f09fde042e0b464228d55236dfd79f
Learning-git <mdayman8162@gmail.com>
2026-04-03 00:27:18 +0600

    completed the frontend navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import profile from "./profile.avif"
+import cart from "./cart.png"
+import menu from "./menu.png"
+import search from "./search.webp"
+
+export const assets = {
+    logo, 
+    profile,
+    cart,
+    menu,
+    search,
+}
