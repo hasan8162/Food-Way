cf8871ef93edb4b89970a06541dcf45170c83a7f
Learning-git <mdayman8162@gmail.com>
2026-04-10 21:12:53 +0600

    created userAuth and logout api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -58,3 +58,29 @@ export const login = async (req, res) => {
         res.json({success: false, message: error.message});
     }
 }
+
+export const isAuth = async (req, res) => {
+    try {
+        const {userId} = req.body;
+        const user = await User.findById(userId).select("-password")
+        return res.json({success: true, user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        });
+
+        return res.json({ success: true, message: "Logged Out" });
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
