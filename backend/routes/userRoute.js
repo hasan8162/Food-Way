ca3a5fa929d583ae0292e50dfd85c374c94691e1
Learning-git <mdayman8162@gmail.com>
2026-04-10 20:08:01 +0600

    completed login and registration api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { register } from '../middleware/userController.js';
+import { register, login } from '../middleware/userController.js';
 
 const userRouter = express.Router();
 userRouter.post('/register', register)
+userRouter.post('/login', login)
 
 export default userRouter;
