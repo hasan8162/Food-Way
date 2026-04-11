33404586d65409db8063ca9a249da864ad7d2892
Learning-git <mdayman8162@gmail.com>
2026-04-11 21:49:52 +0600

    connected admin login page with the backend api

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,6 +3,10 @@ import { createContext, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import {toast} from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 export const AppContextProvider = ({children}) => {
@@ -14,6 +18,20 @@ export const AppContextProvider = ({children}) => {
     const [products, setProducts] = useState([])
     const [cartItems, setCartItems] = useState([])
     const [searchQuery, setSearchQuery] = useState({});
+
+    const fetchSeller = async () => {
+        try {
+            const {data} = await axios.get('/api/seller/is-auth')
+            if(data.success){
+                setIsSeller(true)
+            } else {
+                setIsSeller(false)
+            }
+        } catch (error) {
+            setIsSeller(false)
+        }
+    }
+
     const fetchProducts = async () => {
         setProducts(dummyProducts)
     }
@@ -67,9 +85,10 @@ export const AppContextProvider = ({children}) => {
     }
 
     useEffect(() => {
+        fetchSeller()
         fetchProducts()
     }, [])
-    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount}
+    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount, axios}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
--- frontend/src/pages/seller/SellerLayout.jsx
+++ frontend/src/pages/seller/SellerLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, Navigate, NavLink, Outlet } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const SellerLayout = () => {
 
-    const { setIsSeller } = useAppContext();
+    const { axios, navigate } = useAppContext();
 
     
 
@@ -16,7 +17,18 @@ const SellerLayout = () => {
     ];
 
     const logout = async () => {
-        setIsSeller(false);
+        try {
+            const { data } = await axios.get('/api/seller/logout');
+
+            if(data.success){
+                toast.success(data.message)
+                navigate('/')
+            } else {
+                toast.error(data.message)
+            }
+        } catch(error) {
+            toast.error(error.message)
+        }
     }
 
     return (
